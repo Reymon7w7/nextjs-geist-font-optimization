0e7b82b40f68e6a6656949d5c83bfff0911a32cd
Reymon7w7 <107432593+Reymon7w7@users.noreply.github.com>
2025-07-20 10:30:08 -0400

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753021807979

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d67bee0d._.js",
+  "static/chunks/node_modules_3edb2fc4._.js"
+],
+    source: "dynamic"
+});
